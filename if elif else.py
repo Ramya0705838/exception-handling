5527ef2ff4704bbe4516ec45283e6980a8565a56
ramya <ramiramyasree@gmail.com>
2026-03-24 10:03:18 +0530

    first commit

---
+++ if elif else.py
@@ -0,0 +1,21 @@
+try:
+    num1 = int(input("enter first number:"))
+    num2 = int(input("enter second number:"))
+    print("choose operation: +,-,*,/")
+    op = input("enter operator:")
+    if op == "+":
+        print("result:",num1+num2)
+    elif op == "-":
+        print("result:",num1-num2)
+    elif op == "*":
+        print("result:",num1*num2)
+    elif op == "/":
+        print("result:",num1/num2)
+    else:
+        print("invalid operator")
+except ZeroDivisionError:
+    print("error:cannot divide by zero")
+except ValueError:
+    print("error:value is no defined")
+else:
+    print("calculation successfully done")
